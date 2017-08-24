ee4b71dfaeb5f626e3f5e9759c9b4f9e4e89f2eb
Aden King <adenking@live.com>
2017-08-24 20:08:17 +1000

    setup roman numeral values to compare to

--- Int_To_Roman.py
+++ Int_To_Roman.py
@@ -1,5 +1,8 @@
+roman_values = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC",
+                50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
+# set roman numeral values
+
 def convert_num(num):
+    roman_num = 0
 
-
-    roman_num = num #placeholder
     return roman_num
