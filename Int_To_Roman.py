765359d84b15f597fa343bd0057704f68e1f651b
Aden King <adenking@live.com>
2017-09-13 18:16:41 +1000

    add some commenting

--- Int_To_Roman.py
+++ Int_To_Roman.py
@@ -3,6 +3,7 @@ roman_values = {1000: "M", 900: "CM", 500: "D", 400: "CD", 100: "C", 90: "XC",
                 50: "L", 40: "XL", 10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"}
 
 
+# used to encode ints to roman numerals
 def encode_num(num):
     roman_num = ''
     while num > 0:
@@ -16,6 +17,7 @@ def encode_num(num):
     return roman_num
 
 
+# Used to decode roman numerals to Ints
 def decode_num(num):
     roman_num = ''
     while num > 0:
